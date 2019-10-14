f380bad1220b1fd509d0dd65e58808869c28b379
Jessica Brookhouse <jessicabrookhouse1@gmail.com>
2019-10-14 15:53:29 -0600

    separating helloworld stuff

--- src/components/HexTile.vue
+++ src/components/HexTile.vue
@@ -19,12 +19,12 @@ export default {
 <style>
 .hexagon {
   position: relative;
-  width: 300px; 
-  height: 173.21px;
+  width: 25px; 
+  height: 14.43px;
   background-color: #27ae60;
-  margin: 86.60px 0;
-  border-left: solid 2px #333333;
-  border-right: solid 2px #333333;
+  margin: 7.22px 0;
+  border-left: solid 1px #333333;
+  border-right: solid 1px #333333;
 }
 
 .hexagon:before,
@@ -32,24 +32,25 @@ export default {
   content: "";
   position: absolute;
   z-index: 1;
-  width: 212.13px;
-  height: 212.13px;
+  width: 17.68px;
+  height: 17.68px;
   -webkit-transform: scaleY(0.5774) rotate(-45deg);
   -ms-transform: scaleY(0.5774) rotate(-45deg);
   transform: scaleY(0.5774) rotate(-45deg);
   background-color: inherit;
-  left: 41.9340px;
+  left: 2.6612px;
 }
 
 .hexagon:before {
-  top: -106.0660px;
-  border-top: solid 2.8284px #333333;
-  border-right: solid 2.8284px #333333;
+  top: -8.8388px;
+  border-top: solid 1.4142px #333333;
+  border-right: solid 1.4142px #333333;
 }
 
 .hexagon:after {
-  bottom: -106.0660px;
-  border-bottom: solid 2.8284px #333333;
-  border-left: solid 2.8284px #333333;
+  bottom: -8.8388px;
+  border-bottom: solid 1.4142px #333333;
+  border-left: solid 1.4142px #333333;
 }
+
 </style>
